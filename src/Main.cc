e37343182539a60f64dafaf9f4c52b1a269e5955
Underdisc <undydisc@gmail.com>
2026-01-31 17:51:14 +0100

    Functional Filters

--- src/Main.cc
+++ src/Main.cc
@@ -6,6 +6,7 @@
 #include <comp/Camera.h>
 #include <comp/CameraOrbiter.h>
 #include <comp/Name.h>
+#include <comp/Relationship.h>
 #include <comp/Sprite.h>
 #include <comp/Text.h>
 #include <comp/Transform.h>
@@ -34,26 +35,6 @@
 
 enum class Direction { Up, Right, Down, Left };
 
-struct Digit {
-  int mCell[2];
-  int mValue;
-  Direction mDirection;
-};
-
-struct Filter {
-  enum class Type { Add, Sub, Mul, Div };
-  Type mType;
-  int mCell[2];
-  int mValue;
-  bool mPlaceable;
-};
-
-struct Shifter {
-  int mCell[2];
-  Direction mDirection;
-  bool mPlaceable;
-};
-
 bool nPaused = true;
 float nAutomataTimePassed = 0.9f;
 const Vec3 nFieldOrigin = {0.0f, 0.0f, 0.0f};
@@ -62,7 +43,31 @@ constexpr int nFieldHeight = 10;
 World::MemberId nDigitLayer[nFieldHeight][nFieldWidth];
 World::MemberId nModifierLayer[nFieldHeight][nFieldWidth];
 
+const float nFieldZ = 0.0f;
+const float nModifierZ = 1.0f;
+const float nDigitZ = 2.0f;
+
 World::Object nRunDisplay;
+
+struct Digit {
+  int mCell[2];
+  int mValue;
+  Direction mDirection;
+};
+
+struct Filter {
+  int mCell[2];
+  int mValue;
+  enum class Type { Add, Sub, Mul, Div };
+  Type mType;
+  bool mPlaceable;
+};
+
+struct Shifter {
+  int mCell[2];
+  Direction mDirection;
+  bool mPlaceable;
+};
 
 struct Level {
   Ds::Vector<Digit> mDigits;
@@ -95,6 +100,32 @@ void CreateLevels() {
         .mDirection = Direction::Left,
       },
     };
+    level.mFilters = {
+      {
+        .mCell = {5, 6},
+        .mValue = 3,
+        .mType = Filter::Type::Add,
+        .mPlaceable = false,
+      },
+      {
+        .mCell = {7, 4},
+        .mValue = 1,
+        .mType = Filter::Type::Sub,
+        .mPlaceable = false,
+      },
+      {
+        .mCell = {5, 2},
+        .mValue = 3,
+        .mType = Filter::Type::Mul,
+        .mPlaceable = false,
+      },
+      {
+        .mCell = {3, 4},
+        .mValue = 3,
+        .mType = Filter::Type::Div,
+        .mPlaceable = false,
+      },
+    };
     nLevels.Emplace(std::move(level));
   }
 }
@@ -105,8 +136,12 @@ void UpdateGraphics() {
   for (MemberId memberId: digitIds) {
     const auto& digit = space.Get<Digit>(memberId);
     auto& transform = space.Get<Comp::Transform>(memberId);
-    Vec3 offset = {(float)digit.mCell[0], (float)digit.mCell[1], 1.0f};
+    Vec3 offset = {(float)digit.mCell[0], (float)digit.mCell[1], nDigitZ};
     transform.SetTranslation(nFieldOrigin + offset);
+
+    const auto& relationship = space.Get<Comp::Relationship>(memberId);
+    auto& text = space.Get<Comp::Text>(relationship.mChildren[0]);
+    text.mText = std::to_string(digit.mValue);
   }
 }
 
@@ -114,6 +149,7 @@ void PerformStep() {
   World::Space& space = World::nLayers.Back()->mSpace;
   Ds::Vector<MemberId> digitIds = space.Slice<Digit>();
   for (MemberId memberId: digitIds) {
+    // Update the digits position
     auto& digit = space.Get<Digit>(memberId);
     nDigitLayer[digit.mCell[0]][digit.mCell[1]] = World::nInvalidMemberId;
     switch (digit.mDirection) {
@@ -124,6 +160,31 @@ void PerformStep() {
     }
     digit.mCell[0] = Math::Clamp(0, 9, digit.mCell[0]);
     digit.mCell[1] = Math::Clamp(0, 9, digit.mCell[1]);
+
+    World::MemberId modifierMemberId =
+      nModifierLayer[digit.mCell[0]][digit.mCell[1]];
+    if (modifierMemberId == World::nInvalidMemberId) {
+      continue;
+    }
+    World::Object modifierObj(&space, modifierMemberId);
+    auto* filter = modifierObj.TryGet<Filter>();
+    if (filter != nullptr) {
+      switch (filter->mType) {
+      case Filter::Type::Add:
+        digit.mValue = digit.mValue + filter->mValue;
+        break;
+      case Filter::Type::Sub:
+        digit.mValue = digit.mValue - filter->mValue;
+        break;
+      case Filter::Type::Mul:
+        digit.mValue = digit.mValue * filter->mValue;
+        break;
+      case Filter::Type::Div:
+        digit.mValue = digit.mValue / filter->mValue;
+        break;
+      }
+      digit.mValue %= 10;
+    }
   }
 }
 
@@ -173,7 +234,7 @@ void FieldSetup() {
     for (int j = 0; j < 10; ++j) {
       World::Object gridSquare = field.CreateChild();
       auto& squareTransform = gridSquare.Add<Comp::Transform>();
-      Vec3 offset = {(float)j, (float)i, 0.0f};
+      Vec3 offset = {(float)j, (float)i, nFieldZ};
       squareTransform.SetTranslation(nFieldOrigin + offset);
       squareTransform.SetUniformScale(0.8f);
       auto& squareSprite = gridSquare.Add<Comp::Sprite>();
@@ -207,10 +268,9 @@ void LevelSetup(size_t levelIdx) {
     World::Object digitObject = space.CreateObject();
     digitObject.Add<Digit>() = digit;
     auto& transform = digitObject.Add<Comp::Transform>();
-    Vec3 offset = {(float)digit.mCell[0], (float)digit.mCell[1], 1.0f};
+    Vec3 offset = {(float)digit.mCell[0], (float)digit.mCell[1], nDigitZ};
     transform.SetTranslation(nFieldOrigin + offset);
-    transform.SetUniformScale(0.9f);
-
+    transform.SetUniformScale(0.6f);
     auto& sprite = digitObject.Add<Comp::Sprite>();
     sprite.mMaterialId = "images:DigitBg";
 
@@ -218,16 +278,43 @@ void LevelSetup(size_t levelIdx) {
     auto& textTransform = textChildObject.Add<Comp::Transform>();
     textTransform.SetTranslation({0.0f, -0.25f, 0.1f});
     textTransform.SetUniformScale(0.5f);
-
     auto& text = textChildObject.Add<Comp::Text>();
     text.mColor = {0.0f, 0.0f, 0.0f, 1.0f};
     text.mAlign = Comp::Text::Alignment::Center;
     text.mText = std::to_string(digit.mValue);
   }
 
+  for (const Filter& filter: level.mFilters) {
+    World::Object filterObject = space.CreateObject();
+    filterObject.Add<Filter>() = filter;
+    auto& transform = filterObject.Add<Comp::Transform>();
+    Vec3 offset = {(float)filter.mCell[0], (float)filter.mCell[1], nModifierZ};
+    transform.SetTranslation(nFieldOrigin + offset);
+    transform.SetUniformScale(0.7f);
+    auto& sprite = filterObject.Add<Comp::Sprite>();
+    sprite.mMaterialId = "images:DigitBg";
+
+    World::Object textChildObject = filterObject.CreateChild();
+    auto& textTransform = textChildObject.Add<Comp::Transform>();
+    textTransform.SetTranslation({0.0f, -0.25f, 0.1f});
+    textTransform.SetUniformScale(0.5f);
+    auto& text = textChildObject.Add<Comp::Text>();
+    text.mColor = {0.0f, 0.6f, 0.0f, 1.0f};
+    text.mAlign = Comp::Text::Alignment::Center;
+    std::string filterChar = "";
+    switch (filter.mType) {
+    case Filter::Type::Add: filterChar = "+"; break;
+    case Filter::Type::Sub: filterChar = "-"; break;
+    case Filter::Type::Mul: filterChar = "*"; break;
+    case Filter::Type::Div: filterChar = "/"; break;
+    }
+    text.mText = filterChar + std::to_string(filter.mValue);
+  }
+
   for (int i = 0; i < 10; ++i) {
     for (int j = 0; j < 10; ++j) {
       nDigitLayer[j][i] = World::nInvalidMemberId;
+      nModifierLayer[j][i] = World::nInvalidMemberId;
     }
   }
 
@@ -236,10 +323,17 @@ void LevelSetup(size_t levelIdx) {
     auto& digit = space.Get<Digit>(memberId);
     nDigitLayer[digit.mCell[0]][digit.mCell[1]] = memberId;
   }
+
+  Ds::Vector<MemberId> filterIds = space.Slice<Filter>();
+  for (MemberId memberId: filterIds) {
+    auto& filter = space.Get<Filter>(memberId);
+    nModifierLayer[filter.mCell[0]][filter.mCell[1]] = memberId;
+  }
 }
 
 void RegisterCustomTypes() {
   RegisterComponent(Digit);
+  RegisterComponent(Filter);
 }
 
 int main(int argc, char* argv[]) {
